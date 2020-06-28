78ec5691d8d941528bfe9e28f4fbd865183eb526
liuxiong <flatflash@163.com>
2020-06-29 00:41:03 +0800

    day02

--- Project1/demo1.cpp
+++ Project1/demo1.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
 
 int main() {
+	vector<int> temp{ 1,2,3,4,5,6,7,8 };
+	vector<int>::iterator it = temp.begin();
+	cout << *it << endl;
 	return 0;
 }
